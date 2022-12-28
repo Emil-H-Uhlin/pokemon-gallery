4c61b9790351f5c000989785e08bccb315c96829
Emil <45757491+Emil-H-Uhlin@users.noreply.github.com>
2022-12-28 13:01:15 +0100

    Improved navbar
    Added custom nav-buttons

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,5 @@
+import {useNavigate} from "react-router-dom";
+
 const Header = () => <div className="main-header">
   <div className="logo">
     <img src="/pokemon-logo.png" alt="Pokemon Logo"/>
@@ -5,13 +7,37 @@ const Header = () => <div className="main-header">
   <div className="navbar">
     <ul>
       <li>
-        <button>About</button>
+        <NavButton target={"/home"}>
+          About
+        </NavButton>
       </li>
       <li>
-        <button>Browse</button>
+        <NavButton target={"/browse"}>
+          Browse
+        </NavButton>
       </li>
     </ul>
   </div>
 </div>
+
+function NavButton({target, children:text}: { children: string, target: string}) {
+  const navigate = useNavigate()
+  const active = (() => {
+    const {href:path, host} = window.location
+    return path.substring(path.indexOf(host) + host.length) === target
+  })()
+
+  const navigateToTarget = () => navigate(target)
+
+  return <button
+    onClick={active
+      ? () => { /* no-op */ }
+      : navigateToTarget}
+    className={active
+      ? "active-nav-button"
+      : "nav-button"}>
+    {text}
+  </button>
+}
 
 export default Header
