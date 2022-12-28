42117a7c046f028f69658fc6e137d0a741f20431
Emil Uhlin <emil.h.uhlin@gmail.com>
2022-12-28 11:19:43 +0100

    feature: added navbar to main header currently it is not tied to any routes

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,17 @@
 const Header = () => <div className="main-header">
-  this is the header
+  <div className="logo">
+    <img src="/pokemon-logo.png" alt="Pokemon Logo"/>
+  </div>
+  <div className="navbar">
+    <ul>
+      <li>
+        <button>About</button>
+      </li>
+      <li>
+        <button>Browse</button>
+      </li>
+    </ul>
+  </div>
 </div>
 
 export default Header
